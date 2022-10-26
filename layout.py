a0da8782de86e1137949c1e762c974e13485df0a
commanderjcc <jj1355810@gmail.com>
2022-10-26 17:55:42 -0600

    Refactor into layout.py
    Add accordian to make things pretty

---
+++ layout.py
@@ -0,0 +1,110 @@
+import dash_bootstrap_components as dbc
+from dash import Dash, dcc, html, dash_table
+
+header = [
+    dbc.Row([
+        dbc.Col(
+            dbc.Card([
+                html.H2("Abbott Lab GUI"),
+                html.H5("Coolness overload")
+            ], body="true", color="light"), width={"size": 10, "offset": 1})
+    ]),
+]
+
+localstorage = [
+    dcc.Store(id='memory-output'),
+    dcc.Store(id='history'),
+    dcc.Store(id='updated-table'),
+]
+
+shift_tab = [
+    html.P("Vertical Shift of:"),
+    dcc.Input(id="shift_amount", type="number", placeholder="", style={'width': '100%'}),
+    dbc.Button("Shift", id="shift_button", color="primary",
+               style={'display': 'inline-block', "margin": "5px"},
+               n_clicks=0),
+]
+
+delete_tab = [
+    html.P("Delete Box or Lasso Selection"),
+    dbc.Button("Delete", id="delete", color="primary",
+               style={'display': 'inline-block', "margin": "5px"},
+               n_clicks=0),
+]
+
+compress_tab = [
+    html.P("Compression factor:"),
+    dcc.Input(id="compression_factor", type="number", placeholder="", style={'width': '100%'}),
+    dbc.Button("Expand/Compress", id="compress_button", color="primary",
+               style={'display': 'inline-block', "margin": "5px"},
+               n_clicks=0),
+]
+
+export_tab = [
+    html.P("Export as CSV"),
+    dcc.Input(id="export_filename", type="text", placeholder="export.csv", style={'width': '100%'}),
+    dbc.Button("Export", id="exportDF", color="primary",
+               style={'display': 'inline-block', "margin": "5px"},
+               n_clicks=0),
+]
+
+editor = [
+    # dbc.Card([
+    #     dbc.CardHeader([
+            dbc.Accordion([
+                dbc.AccordionItem(shift_tab, title="Shift︎"),
+                dbc.AccordionItem(compress_tab, title="Compress"),
+                dbc.AccordionItem(delete_tab, title="Delete"),
+                dbc.AccordionItem(export_tab, title="Export"),
+            ], start_collapsed=True),
+    #     ]),
+    #
+    #     dbc.CardBody(id="editor_card_body")
+    # ])
+]
+
+layout = dbc.Container([
+    *localstorage,
+    *header,
+    html.Hr(),
+    dbc.Row([
+        dbc.Col([
+            dbc.Card([
+                html.H5("Run Site Query"),
+                "Site ID:",
+                dcc.Dropdown(
+                    options=['BEN', 'BLI', 'BSL', 'CLE', 'CRB', 'DAI', 'DFF', 'DFL', 'DFM', 'DFU', 'HCL',
+                             'HCN', 'HCS', 'IND', 'LAK', 'LDF', 'MIT', 'NEB', 'PBC', 'SBL', 'SFL', 'SHE',
+                             'SOL', 'STR', 'TCU', 'TIE', 'WAN'],
+                    value='BEN',
+                    id='site_id',
+                    style={'display': 'inline-block', "width": "80%", "margin": "2px"}),
+                dbc.Button("Query Site", id="query", color="primary",
+                           style={'display': 'inline-block', "margin": "5px"},
+                           n_clicks=0)
+            ], body="true", color="light"),
+            html.Hr(),
+            *editor,
+        ], width=3),
+        dbc.Col(
+            dbc.Card(
+                dcc.Graph(id='indicator-graphic'), body='True', color="light"), width=9)
+    ]),
+    html.Hr(),
+    dbc.Row([
+        dbc.Col([
+            dbc.Card([
+                dcc.Markdown("""
+                    **Click Data**
+
+                    Click on a point from the graph to display more about that observation.
+                """),
+                html.Pre(id='selected'),
+            ], body="true", color="light")
+        ], width=3),
+        dbc.Col([
+            dbc.Card([html.Div(id="update-table"),
+                      ], body="true", color="light")
+        ], width=9)
+    ])
+])
